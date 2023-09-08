163400fe562c29d32b8e96508bcc98dd1ad338c0
kim yungu <ygkim@unipoint.co.kr>
2023-09-08 17:03:23 +0900

    게시판 완료

---
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -0,0 +1,55 @@
+package com.cos.blog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.blog.model.Board;
+import com.cos.blog.model.User;
+import com.cos.blog.repository.BoardRepository;
+
+//서비스는 하나의 트랜잭션(작업 단위)면 상관이 없지만 두 개 이상의 트랜잭션을 한번에 수행하기 위하여 사용한다.
+//스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다.
+@Service
+public class BoardService {
+
+		@Autowired
+		private BoardRepository boardRepository;
+		
+		@Transactional
+		public void 글쓰기(Board board,User user) {
+				board.setCount(0);
+				board.setUser(user);
+				boardRepository.save(board);
+		}
+		@Transactional(readOnly = true)
+		public Page<Board> 글목록(Pageable pageable){
+			return boardRepository.findAll(pageable);
+		};
+		@Transactional(readOnly = true)
+		public Board 글상세보기(int id) {
+			return boardRepository.findById(id)
+					.orElseThrow(()->{
+						return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.");
+					});
+		}
+		
+		@Transactional
+		public void 삭제하기(int id) {
+			System.out.println("글 삭제하기 : " + id);
+				boardRepository.deleteById(id);
+		}
+		@Transactional
+		public void 글수정하기(int id,Board requestBoard) {
+			Board board = boardRepository.findById(id)
+					.orElseThrow(()->{
+						return new IllegalArgumentException("글 수정 실패: 아이디를 찾을 수 없습니다.");
+					}); // 영속화 완료
+			board.setTitle(requestBoard.getTitle());
+			board.setContent(requestBoard.getContent());
+			// 해당 함수로 종료시 ( service 종료될 때) 알아서 업데이트 : 더티채킹 -> 자동 업데이트가 됨 flush
+		}
+		
+}
---
+++ src/main/resources/static/js/board.js
@@ -0,0 +1,77 @@
+let index = {
+	init: function(){
+		$("#btn-save").on("click",()=>{ 
+			this.save();
+		});
+		$("#btn-delete").on("click",()=>{ 
+			this.deleteById();
+		});
+		$("#btn-update").on("click",()=>{ 
+			this.update();
+		});
+	},
+	
+	save: function() {
+		let data = {
+			title:$("#title").val(),
+			content:$("#content").val(),
+		};
+		
+		$.ajax({
+			type: "POST",
+			url: "/api/board", 
+			data: JSON.stringify(data), // http body 데이터
+			contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트로 변경
+		}).done(function(resp){ // 성공
+			alert("글 작성이 완료되었습니다.");
+			console.log(resp);
+			location.href="/";
+		}).fail(function(error){ //실패
+			alert(JSON.stringify(error));
+		});  // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
+		
+	},
+	
+	deleteById: function() {
+		let id = $("#id").text();
+		
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/"+id, 
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트로 변경
+		}).done(function(resp){ // 성공
+			alert("삭제가 완료되었습니다.");
+			console.log(resp);
+			location.href="/";
+		}).fail(function(error){ //실패
+			alert(JSON.stringify(error));
+		}); 
+		
+	},
+	
+	update: function() {
+		let id = $("#id").val();
+		
+		let data = {
+			title:$("#title").val(),
+			content:$("#content").val(),
+		};
+		$.ajax({
+			type: "PUT",
+			url: "/api/board/" +id, 
+			data: JSON.stringify(data), // http body 데이터
+			contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지 (MIME)
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트로 변경
+		}).done(function(resp){ // 성공
+			alert("글 수정이 완료되었습니다.");
+			console.log(resp);
+			location.href="/";
+		}).fail(function(error){ //실패
+			alert(JSON.stringify(error));
+		});  
+		
+	},
+	
+}
+index.init();
